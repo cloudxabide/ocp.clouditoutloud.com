6989e09a2495ed612e8b831aef2c78bcc4b69b28
James Radtke <cloudxabide@gmail.com>
2023-03-04 09:14:24 -0600

    Generic Update

--- Scripts/21-Identity_Providers.sh
+++ Scripts/21-Identity_Providers.sh
@@ -1,14 +1,16 @@
 # Identity Providers
 
+cd ${OCP4_BASE}
+
 # HTPASSWORD=""
-HTPASSWD_FILE=${OCP4_DIR}/htpasswd
+HTPASSWD_FILE=htpasswd
 
 htpasswd -b -c $HTPASSWD_FILE morpheus $HTPASSWORD
 htpasswd -b $HTPASSWD_FILE ocpguest $HTPASSWORD
 htpasswd -b $HTPASSWD_FILE ocpadmin $HTPASSWORD
 
 oc create secret generic htpass-secret --from-file=htpasswd=${HTPASSWD_FILE} -n openshift-config
-cat << EOF > ${OCP4_DIR}/idp-cr-HTPasswd.yaml
+cat << EOF > ./idp-cr-HTPasswd.yaml
 ---
 apiVersion: config.openshift.io/v1
 kind: OAuth
@@ -24,12 +26,14 @@ spec:
         name: htpass-secret
 EOF
 
-oc apply -f ${OCP4_DIR}/idp-cr-HTPasswd.yaml
+oc apply -f ./idp-cr-HTPasswd.yaml
 # Need to automate this wait-period and then status check (to make sure pods have restarted)
 sleep 90
 
 # Watch the pods until they "re-settle" before proceeding - usually like 2 minutes?
 oc get pods -n openshift-authentication -w
+# You need to login to the cluster before you can modify the user permissions 
+#   (either run the following, or login to the web console)
 sudo su - -c "oc login -u ocpadmin -p $HTPASSWORD --server=$OCP4API:$OCP4APIPORT"
 oc adm policy add-cluster-role-to-user cluster-admin ocpadmin
 
